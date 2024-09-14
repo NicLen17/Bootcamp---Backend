02b7ec3c0cb8ed39508fc02f06420af612a9b635
Nicolas Powell <nicopowell30@gmail.com>
2024-09-14 04:45:27 -0300

    se creo el controlador para traer los cursos

---
+++ src/controllers/cursos.controllers.js
@@ -0,0 +1,15 @@
+serviciosDeCursos = require('../services/cursos.services')
+
+const obtenerTodosLosCursos = async (req, res) => {
+    const result = await serviciosDeCursos.obtenerTodosLosCursos()
+
+    if (result.statusCode === 200){
+        res.status(200).json(result.cursos)
+    } else {
+        res.status(500).json({msg: result.msg})
+    }
+}
+
+module.exports = {
+  crearCurso
+}
