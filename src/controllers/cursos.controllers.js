8067333a450423219c7d6c1a9a64658778b0466d
Nicolas Powell <nicopowell30@gmail.com>
2024-09-14 05:02:04 -0300

    se creo el controlador para obtener un curso

---
+++ src/controllers/cursos.controllers.js
@@ -0,0 +1,15 @@
+serviciosDeCursos = require('../services/cursos.services')
+
+const obtenerUnCurso = async (req, res) => {
+    const result = await serviciosDeCursos.obtenerUnCurso(req.params.idProducto)
+
+    if (result.statusCode === 200){
+        res.status(200).json(result.curso)
+    } else {
+        res.status(500).json({msg: result.msg})
+    }
+}
+
+module.exports = {
+  obtenerUnCurso
+}
