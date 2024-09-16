b9ee9bf97a39a15828396d5d7ed99ac296f92f4a
unknown <fabioramosnic@gmail.com>
2024-09-16 02:25:24 -0300

    se agrego la funcionalidad de editar un curso

--- src/controllers/cursos.controllers.js
+++ src/controllers/cursos.controllers.js
@@ -33,9 +33,19 @@ const eliminarCurso = async (req, res) => {
     const id = req.params.idCurso
     const result = await serviciosDeCursos.eliminarCurso(id)
     if (result.statusCode === 200) {
-        res.status(200).json({ msg: "Curso eliminado con éxito" })
+        res.status(200).json({ msg: result.msg })
     } else {
         res.status(500).json({ msg: result.msg })
+    }
+}
+
+const editarCurso = async (req, res) => {
+    const id = req.params.idCurso
+    const result = await serviciosDeCursos.editarCurso(id, req.body)
+    if (result.statusCode === 200) {
+        res.status(200).json({ msg: result.msg })
+    } else {
+        res.status(result.statusCode).json({ msg: result.msg })
     }
 }
 
@@ -43,5 +53,6 @@ module.exports = {
   obtenerTodosLosCursos,
   obtenerUnCurso,
   crearCurso,
-  eliminarCurso
+  eliminarCurso,
+  editarCurso   
 }
