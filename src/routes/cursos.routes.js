40cb002b43d8e839294808a9e6f82513c9d378b7
Nicolas Powell <nicopowell30@gmail.com>
2024-09-17 16:38:52 -0300

    se creo la ruta para agregar una imagen a un curso

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { obtenerTodosLosCursos, obtenerUnCurso, crearCurso, eliminarCurso, editarCurso } = require('../controllers/cursos.controllers')
+const { obtenerTodosLosCursos, obtenerUnCurso, crearCurso, agregarImagenCurso, eliminarCurso, editarCurso } = require('../controllers/cursos.controllers')
+const multer = require('multer')
 const router = express.Router()
 
 router.get('/', obtenerTodosLosCursos)
 router.get('/:idCurso', obtenerUnCurso)
+router.post('/', crearCurso)
+router.post('/agregarImagen/:idCurso', multer.single('imagen'), agregarImagenCurso)
 router.delete('/:idCurso', eliminarCurso)
 router.put('/:idCurso', editarCurso)
-router.post('/', crearCurso)
 
 module.exports = router
