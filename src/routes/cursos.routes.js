608aff7d0ca534d2b7b65448ded2f880e76c8dad
Nicolas Powell <nicopowell30@gmail.com>
2024-09-18 21:53:55 -0300

    se creó la ruta para habilitar/deshabilitar un curso

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { obtenerTodosLosCursos, obtenerUnCurso, crearCurso, agregarImagenCurso, eliminarCurso, editarCurso } = require('../controllers/cursos.controllers')
+const { obtenerTodosLosCursos, obtenerUnCurso, crearCurso, agregarImagenCurso, eliminarCurso, editarCurso, cambiarEstadoCurso } = require('../controllers/cursos.controllers')
 const multer = require('../middlewares/multer')
 const auth = require('../middlewares/auth')
 const router = express.Router()
@@ -10,5 +10,6 @@ router.get('/', obtenerTodosLosCursos)
 router.get('/:idCurso', obtenerUnCurso)
 router.delete('/:idCurso', auth('admin'), eliminarCurso)
 router.put('/:idCurso', auth('admin'), editarCurso)
+router.put('/estado/:idCurso', auth('admin'), cambiarEstadoCurso)
 
 module.exports = router
