c93eda4cb1b0cc702704724d50152212289c2724
Joaset <kevinstephantorres@gmail.com>
2024-09-26 03:17:23 -0300

    Se creo los controllers para la compra de cursos

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -93,7 +93,15 @@ const obtenerCarrito = async (req, res) => {
   }
 }
 
+const comprar = async (req, res) => {
+  const result = await serviceUsuario.comprar(req.idUsuario)
 
+  if(result.statusCode === 200){
+      res.status(200).json({msg: result.msg})
+  }else{
+    res.status(500).json({msg: result.msg})
+  }
+}
 
 module.exports = {
     listarUsuarios,
@@ -103,5 +111,6 @@ module.exports = {
     cambiarEstadoUsuario,
     editarUsuario,
     eliminarUsuario,
-    obtenerCarrito
+    obtenerCarrito,
+    comprar
 }
