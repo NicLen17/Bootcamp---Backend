2c5f341ddae715ab0654b7cc74dd400303d8a3ac
Nicolas Powell <nicopowell30@gmail.com>
2024-09-30 22:15:55 -0300

    se creo la ruta para obtener la valoracion general

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { obtenerTodosLosCursos, obtenerUnCurso, crearCurso, agregarImagenCurso, eliminarCurso, editarCurso, agregarEliminarCursoCarrito, cambiarEstadoCurso, obtenerTodosLosCursosHabilitados, whatsAppApi, puntuarCurso, obtenerTodasLasValoraciones } = require('../controllers/cursos.controllers')
+const { obtenerTodosLosCursos, obtenerUnCurso, crearCurso, agregarImagenCurso, eliminarCurso, editarCurso, agregarEliminarCursoCarrito, cambiarEstadoCurso, obtenerTodosLosCursosHabilitados, whatsAppApi, puntuarCurso, obtenerTodasLasValoraciones, obtenerValoracionGeneral } = require('../controllers/cursos.controllers')
 
 const multer = require('../middlewares/multer')
 const auth = require('../middlewares/auth')
@@ -17,5 +17,6 @@ router.put('/estado/:idCurso', auth('admin'), cambiarEstadoCurso)
 router.post('/whatsapp', whatsAppApi)
 router.post('/puntuar/:idCurso', auth('usuario'), puntuarCurso)
 router.get('/valoraciones/:idCurso', auth('admin'), obtenerTodasLasValoraciones)
+router.get('/valoracionGeneral/:idCurso', obtenerValoracionGeneral)
 
 module.exports = router
