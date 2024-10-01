ef3f55a0337025626057e465fdc0d9252c00ee32
Joaset <kevinstephantorres@gmail.com>
2024-09-30 23:37:10 -0300

    Se agrego validaciones para la contraseña en el Schema del usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -10,13 +10,14 @@ router.post('/', [
     check('email', 'Campo Email vacío').notEmpty(),
     check('email', 'Introduce un correo válido').isEmail(),
     check('email', 'El correo debe tener entre 10 y 50 caracteres').isLength({ min: 10, max: 50 }),
-    check('password', 'Campo Contraseña vacío').notEmpty(), // faltan mas validaciones en el password
+    check('password', 'Campo Contraseña vacío').notEmpty(),
+     
     check('rol', 'El rol debe ser admin o usuario').optional().isIn(['admin', 'usuario'])
 ], crearUsuario)
 router.post('/login', [
     check('nombre', 'Campo Nombre de Usuario vacío').notEmpty(),
     check('nombre', 'El nombre debe tener entre 4 y 20 caracteres').isLength({ min: 4, max: 20 }),
-    check('password', 'Campo Contraseña vacío').notEmpty(), // faltan mas validaciones en el password
+    check('password', 'Campo Contraseña vacío').notEmpty(), 
 ],inicioSesion)
 router.get('/', auth('admin'), listarUsuarios)
 router.get('/carrito', auth('usuario'), obtenerCarrito)
