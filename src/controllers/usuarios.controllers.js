2a725b0b04e507f7a2d51f118e62a535854aec41
Nicolas Powell <nicopowell30@gmail.com>
2024-09-30 21:23:28 -0300

    se creo el controlador para puntuar un curso

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -103,6 +103,16 @@ const comprar = async (req, res) => {
   }
 }
 
+const puntuarCurso = async (req, res) => {
+  const result = await serviceUsuario.puntuarCurso(req.idUsuario, req.params.idCurso, req.body)
+
+  if(result.statusCode === 200){
+      res.status(200).json({msg: result.msg})
+  }else{
+    res.status(500).json({msg: result.msg})
+  }
+}
+
 module.exports = {
     listarUsuarios,
     obtenerUsuario,
@@ -112,5 +122,6 @@ module.exports = {
     editarUsuario,
     eliminarUsuario,
     obtenerCarrito,
-    comprar
+    comprar,
+    puntuarCurso
 }
