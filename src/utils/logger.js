f5050ea836a44cd772f4f43f0ee42339a9a5d937
Joaset <kevinstephantorres@gmail.com>
2024-10-01 23:36:55 -0300

    Se agrego formato a Winston

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,8 @@ const {createLogger, format, transports} = require('winston')
 module.exports = createLogger({
     transports:[
         new transports.Console({
-            level: 'debug'
+            level: 'debug',
+            format: format.combine(format.simple())
         })
     ]
 })
