caa260b7bd10d2913e7533763b7f1ad5b8eadcda
Joaset <kevinstephantorres@gmail.com>
2024-10-02 00:27:44 -0300

    Se modifico el formato del logger y se creo una carpeta log donde se va a crer el archivo combined.log que tendra todo lo que se mande por logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,10 +1,15 @@
 const {createLogger, format, transports} = require('winston')
 
 module.exports = createLogger({
-    transports:[
-        new transports.Console({
-            level: 'debug',
-            format: format.combine(format.simple())
+    format: format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Formato de fecha y hora
+        format.printf(({ timestamp, level, message }) => {
+            return `${timestamp} [${level}]: ${message}`; // Mensaje formateado
         })
+    ),
+    level: 'debug',
+    transports:[
+        new transports.Console(),
+        new transports.File({ filename: `${__dirname}/../logs/combined.log` }),
     ]
 })
